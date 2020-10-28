4774de4c26b1620f5eda0686d21b5e91a7f05f95
Pushpendra <pushpendra@localhost.localdomain>
2020-10-28 14:02:34 +0530

    Added a Procfile.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const createError = require("http-errors");
 const express = require("express");
+const favicon = require('serve-favicon');
 
 //Set up mongoose connection
 const mongoose = require("mongoose");
@@ -29,6 +30,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+ 
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+ 
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
