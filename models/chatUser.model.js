2d7bd20d116e524f8d1872eff4aef60560ee434f
Pushpendra Upadhyay <pushpendraupadhyay6452@gmail.com>
2021-12-24 00:22:19 +0530

    Added password field in chatUser model

--- models/chatUser.model.js
+++ models/chatUser.model.js
@@ -5,13 +5,13 @@ const Schema = mongoose.Schema;
 const opts = { toJSON: { virtuals: true } , toObject : {virtuals:true} };
 
 const ChatUserSchema = new Schema({
-  name: { type: String, required: false, maxlength: 200 },
   phone: { type: String, required: true, maxlength: 20 },
+  password: { type: Object, required: true },
+  name: { type: String, required: false, maxlength: 200 },
   about: { type: String, required: false, maxlength: 500 },
   created_at : { type: Date, default: Date.now },
   updated_at : { type: Date, default: Date.now },
 },opts);
 
 //Export model
-
 module.exports = mongoose.model('ChatUser', ChatUserSchema);
