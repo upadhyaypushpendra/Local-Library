6af31c485d44cc56a1cf431f07653afe1636c059
Pushpendra Upadhyay <pushpendraupadhyay6452@gmail.com>
2021-12-24 02:01:35 +0530

    Update chatUser.js

--- routes/chatUser.js
+++ routes/chatUser.js
@@ -4,22 +4,31 @@ const router = express.Router();
 //Require controller modules
 const chatUser_controller = require("./../controllers/chatUser.controller");
 
+//Required middlewares
+const authentication = require("./../middleware/authentication");
+
 /* ***chatUser ROUTES*** */
 
 //POST request for creating a chatUser.
-router.post("/create", chatUser_controller.chatUser_create);
+router.post("/signup", chatUser_controller.chatUser_create);
+
+//POST signin chatUser
+router.post("/signin", chatUser_controller.chatUser_signin);
+
+//GET access tokens
+router.get("/access", authentication.authenticateRefreshToken, chatUser_controller.chatUser_get_access_token);
 
 //POST request to delete chatUser
-router.delete("/:id", chatUser_controller.chatUser_delete);
+router.delete("/:id", authenticate.authenticateAccessToken, chatUser_controller.chatUser_delete);
 
 //GET request to update chatUser
-router.put("/:id", chatUser_controller.chatUser_update);
+router.put("/:id", authenticate.authenticateAccessToken, chatUser_controller.chatUser_update);
 
 //GET request to get 1 chatUser
-router.get("/:id", chatUser_controller.chatUser_get);
+router.get("/:id", authenticate.authenticateAccessToken, chatUser_controller.chatUser_get);
 
 //GET request to get all chatUsers
-router.get("/", chatUser_controller.chatUser_list);
+router.get("/", authenticate.authenticateAccessToken, chatUser_controller.chatUser_list);
 
 // Export router
 module.exports = router;
