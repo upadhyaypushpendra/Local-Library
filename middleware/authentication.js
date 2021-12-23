895598538b0719ce6504e8eb56e91d77d8ff86a0
Pushpendra Upadhyay <pushpendraupadhyay6452@gmail.com>
2021-12-24 01:43:16 +0530

    Create authentication.js

---
+++ middleware/authentication.js
@@ -0,0 +1,31 @@
+exports.authenticateAccessToken = (request, response, next) => {
+    const token = request.headers["kutuki-access-token"];
+
+    if (!token) {
+        response.status(401).json(new Error("Token not provided"));
+        return;
+    }
+
+    verifyAccessToken(token).then(userId => {
+        request.headers.userId = userId;
+        next();
+    }).catch(err => {
+        response.status(403).json(err);
+    })
+}
+
+exports.authenticateRefreshToken = (request, response, next) => {
+    const token = request.headers["kutuki-refresh-token"];
+
+    if (!token) {
+        response.status(401).json(new Error("Token not provided"));
+        return;
+    }
+
+    verifyAccessToken(token).then(userId => {
+        request.headers.userId = userId;
+        next();
+    }).catch(err => {
+        response.status(403).json(err);
+    })
+}
