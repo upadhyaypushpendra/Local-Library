fb2044a016aaf86a8f4d2821039d358bebe9b93f
Pushpendra Upadhyay <pushpendraupadhyay6452@gmail.com>
2021-12-23 20:23:22 +0530

    Updated chatUser routes

--- routes/chatUser.js
+++ routes/chatUser.js
@@ -2,21 +2,21 @@ const express = require("express");
 const router = express.Router();
 
 //Require controller modules
-const chatUser_controller = require("./../controllers/chatUser.controller");
+const chatUser_controller = require("./../controllers.controller");
 
 /* ***chatUser ROUTES*** */
 
 //POST request for creating a chatUser.
-router.post("/chatUser/create", chatUser_controller.chatUser_create);
+router.post("/create", chatUser_controller.chatUser_create);
 
 //POST request to delete chatUser
-router.delete("/chatUser/:id", chatUser_controller.chatUser_delete);
+router.delete("/:id", chatUser_controller.chatUser_delete);
 
 //GET request to update chatUser
-router.put("/chatUser/:id", chatUser_controller.chatUser_update);
+router.put("/:id", chatUser_controller.chatUser_update);
 
 //GET request to get 1 chatUser
-router.get("/chatUser/:id", chatUser_controller.chatUser_get);
+router.get("/:id", chatUser_controller.chatUser_get);
 
 //GET request to get all chatUsers
-router.get("/chatUser", chatUser_controller.chatUser_list);
+router.get("/", chatUser_controller.chatUser_list);
